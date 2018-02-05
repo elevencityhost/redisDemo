51943ebb530de3e0c2e80ce20599074e13055118
elevencityhost <15901506742@163.com>
2018-02-05 15:40:42 +0800

    add class to store keys

---
+++ src/main/java/com/example/Teacher.java
@@ -0,0 +1,46 @@
+package com.example;
+
+/**
+ * function
+ *
+ * @author 十一城城主
+ * @data 2018/2/5 15:33
+ */
+public class Teacher {
+    private String id;
+    private String name;
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
